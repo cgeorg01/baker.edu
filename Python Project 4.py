38089456edbe697e47157ae828b9ce1000336879
C. George <cgeorg01@baker.edu>
2024-10-03 10:05:14 -0400

    Add files via upload

--- Python Project 4.py
+++ Python Project 4.py
@@ -4,13 +4,15 @@ def main():
 
     #Get user input
     while i < 3:
-        display_text = 'Enter string ' + str(i + 1) + ':'   #Display text
+        display_text = 'Enter string #' + str(i + 1) + ':'  #Display text
         user_input.append(input(display_text))              #Add user input to list
         i += 1
 
-    print(reverse_string(user_input[0], user_input[1], user_input[2]))  #Print concatenated & reverses string
+    new_string = reverse_string(user_input[0], user_input[1], user_input[2])    #Concatenated & reverses string
 
-#Reverse string function
+    print(new_string)
+
+#Concatenate & Reverse string function
 def reverse_string(s1, s2, s3):
     concat_strings = s1 + s2 + s3   #Concatenate string
     return concat_strings[::-1]     #Return reversed string
