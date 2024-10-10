f8fedf5fbf69e8179f15a95eca46b078ec9daa9b
C. George <cgeorg01@baker.edu>
2024-10-10 12:09:25 -0400

    Add files via upload

--- Python Project 6.py
+++ Python Project 6.py
@@ -1,26 +1,27 @@
-# Convert temperature input to floating point number or quit program.
-def convert_float(x):
-    # Quit program.
-    if x.lower() == 'quit':
-        return exit()
-    # Convert to floating point number.
-    else:
-        # Handle exception.
-        try:
-            temp_float = float(x)
-            return temp_float
-        except ValueError:
-            raise ValueError('Input Exception! Please enter a valid temperature.')
+# Quit program.
+def end(q):
+    if q.lower() == 'quit':
+        return exit('Goodbye!')
 
-# Convert temperature to Fahrenheit.
-def conv_to_f(celsius):
-    f = (9 / 5) * celsius + 32
-    return f
+def convert(temperature, measurement):
+    # Convert temperature input to floating point number.
+    try:
+        temp_float = float(temperature)
+    except ValueError:
+        raise ValueError('Input Exception! Please enter a valid temperature.')
 
-# Convert temperature to Celsius.
-def conv_to_c(fahrenheit):
-    c = (5 / 9) * (fahrenheit - 32)
-    return c
+    # Convert temperature to Fahrenheit or Celsius.
+    try:
+        if measurement.capitalize() == 'C':
+            f = (9 / 5) * temp_float + 32
+            print(f'{temperature} Celsius is {f:.2f} Fahrenheit\n')
+        elif measurement.capitalize() == 'F':
+            c = (5 / 9) * (temp_float - 32)
+            print(f'{temperature} Fahrenheit is {c:.2f} Celsius\n')
+        else:
+            raise ValueError('Input Exception! Please enter a valid unit of measure.')
+    except ValueError as e:
+        print(e, '\n')
 
 def main():
     # Loop program until user input quit.
@@ -28,27 +29,14 @@ def main():
         # Handle exception.
         try:
             # Get temperature.
-            user_temperature = convert_float(input('Enter temperature or quit to exit program: '))
+            user_temperature = input('Enter temperature / Enter quit to exit: ')
+            end(user_temperature)
 
-            i = 0
+            # Get unit of measurement.
+            unit_measurement = input('Enter unit of measure (C = Celsius or F = Fahrenheit): ')
 
-            # Loop until user enters correct unit of measurement.
-            while i == 0:
-                # Get unit of measurement.
-                unit_measurement = input('Enter unit of measure (C or F): ').capitalize()
-
-                # Convert Celsius to Fahrenheit.
-                if unit_measurement.capitalize() == 'C':
-                    i = 1   # end loop
-                    f = conv_to_f(user_temperature)
-                    print(f'{user_temperature} Celsius is {f:.2f} Fahrenheit\n')
-                # Convert Fahrenheit to Celsius.
-                elif unit_measurement.capitalize() == 'F':
-                    i = 1   # end loop
-                    c = conv_to_c(user_temperature)
-                    print(f'{user_temperature} Fahrenheit is {c:.2f} Celsius\n')
-                else:
-                    print('Invalid unit of measurement!\n')
+            #Convert temperature.
+            convert(user_temperature, unit_measurement)
 
         except ValueError as e:
             print(e, '\n')
